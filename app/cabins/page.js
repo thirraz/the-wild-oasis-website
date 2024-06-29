856f0d833930975ec39d048aced4fd33015cde7f
Thiago Souza <thiagofernandosouza20@gmail.com>
2024-06-29 08:12:57 -0300

    feature: adding interactivity with client components

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,3 +1,5 @@
+import Counter from "../components/Counter"
+
 export default async function Page() {
 	const res = await fetch("https://jsonplaceholder.typicode.com/users")
 	const data = await res.json()
@@ -13,6 +15,8 @@ export default async function Page() {
 					<li key={user.id}>{user.name}</li>
 				))}
 			</ul>
+
+			<Counter />
 		</div>
 	)
 }
