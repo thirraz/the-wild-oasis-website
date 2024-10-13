c17339e42571067f15e93a41e69283f8eed05e2d
Thiago Morais <thiagofernandosouza20@gmail.com>
2024-10-13 11:05:14 -0300

    update: setting fonts and improving navigation

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,16 @@ import Navigation from "@/app/_components/Navigation"
 
 import "@/app/_styles/globals.css"
 
+import { Josefin_Sans } from "next/font/google"
+import Header from "./_components/Header"
+
+const josefin = Josefin_Sans({
+	subsets: ["latin"],
+	display: "swap"
+})
+
+console.log(josefin)
+
 export const metadata = {
 	title: {
 		template: "%s / The Wild Oasis",
@@ -15,14 +25,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en-US">
-			<body className="bg-primary-950 text-primary-100 min-h-screen">
-				<header>
-					<Logo />
-					<Navigation />
-				</header>
-				<main>{children}</main>
-				<footer>Copyright by The Wild Oasis</footer>
+			<body
+				className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
+			>
+				<Header />
+				<div className="flex-1 px-8 py-12">
+					<main className="max-w-7xl bg-red-500 mx-auto">{children}</main>
+				</div>
 			</body>
+			lass
 		</html>
 	)
 }
