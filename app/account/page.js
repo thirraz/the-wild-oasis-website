ada2896acc47eb3a055faac080417e18f9e09def
Thiago Morais <thiagofernandosouza20@gmail.com>
2024-10-14 02:41:21 -0300

    update: nested a nested route

--- app/account/page.js
+++ app/account/page.js
@@ -5,7 +5,9 @@ export const metadata = {
 export default function Page() {
 	return (
 		<div>
-			<h1>Account page</h1>
+			<h2 className="font-semibold text-2xl text-accent-400 mb-7">
+				Welcome Jonas!
+			</h2>
 		</div>
 	)
 }
