1e7f77fcb62ea67764e9a716fdac7e5cbc65580b
Thiago Morais <thiagofernandosouza20@gmail.com>
2024-11-22 11:29:47 -0300

    update: creating an API endpoint with route handlers

---
+++ app/api/cabins/[cabinId]/route.js
@@ -0,0 +1,16 @@
+import { getCabin, getBookedDatesByCabinId } from "../../../_lib/data-service"
+
+export async function GET(req, { params }) {
+	const { cabinId } = params
+
+	try {
+		const [cabin, bookedDates] = await Promise.all([
+			getCabin(cabinId),
+			getBookedDatesByCabinId(cabinId)
+		])
+
+		return Response.json({ cabin, bookedDates })
+	} catch {
+		return Response.json({ message: "Cabin not found" })
+	}
+}
