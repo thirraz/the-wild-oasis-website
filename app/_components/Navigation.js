60d3d1a929dccccab2de43246088ab4286f78fd7
Thiago Morais <thiagofernandosouza20@gmail.com>
2024-11-24 16:22:06 -0300

    feature: logged in and out user and content

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,6 +1,11 @@
 import Link from "next/link"
+import { auth } from "@/app/_lib/auth"
 
-export default function Navigation() {
+export default async function Navigation() {
+	const session = await auth()
+
+	console.log(session)
+
 	return (
 		<nav className="z-10 text-xl">
 			<ul className="flex gap-16 items-center">
@@ -21,12 +26,27 @@ export default function Navigation() {
 					</Link>
 				</li>
 				<li>
-					<Link
-						href="/account"
-						className="hover:text-accent-400 transition-colors"
-					>
-						Guest area
-					</Link>
+					{session?.user?.image ? (
+						<Link
+							href="/account"
+							className="hover:text-accent-400 transition-colors flex items-center gap-4"
+						>
+							<img
+								src={session.user.image}
+								alt={`${session.user.name} profile picture`}
+								className="h-8 rounded-full "
+								referrerPolicy="no-referrer"
+							/>
+							<span>{session.user.name}</span>
+						</Link>
+					) : (
+						<Link
+							href="/account"
+							className="hover:text-accent-400 transition-colors"
+						>
+							Guest area
+						</Link>
+					)}
 				</li>
 			</ul>
 		</nav>
--- app/_components/ReservationForm.js
+++ app/_components/ReservationForm.js
@@ -2,7 +2,7 @@
 
 import { useReservation } from "@/app/_contexts/ReservationContext"
 
-function ReservationForm({ cabin }) {
+function ReservationForm({ cabin, user }) {
 	const { range } = useReservation()
 
 	// CHANGE
@@ -13,16 +13,16 @@ function ReservationForm({ cabin }) {
 			<div className="bg-primary-800 text-primary-300 px-16 py-2 flex justify-between items-center">
 				<p>Logged in as</p>
 
-				{/* <div className='flex gap-4 items-center'>
-          <img
-            // Important to display google profile images
-            referrerPolicy='no-referrer'
-            className='h-8 rounded-full'
-            src={user.image}
-            alt={user.name}
-          />
-          <p>{user.name}</p>
-        </div> */}
+				<div className="flex gap-4 items-center">
+					<img
+						// Important to display google profile images
+						referrerPolicy="no-referrer"
+						className="h-8 rounded-full"
+						src={user.image}
+						alt={user.name}
+					/>
+					<p>{user.name}</p>
+				</div>
 			</div>
 
 			<p>
