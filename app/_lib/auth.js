8a44c67cc32e603ee2ba609ef59802f1c88a5e43
Thiago Morais <thiagofernandosouza20@gmail.com>
2024-11-24 17:01:35 -0300

    feature: protecting routes with nextauth middleware

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -7,7 +7,12 @@ const authConfig = {
 			clientId: process.env.AUTH_GOOGLE_ID,
 			clientSecret: process.env.AUTH_GOOGLE_SECRET
 		})
-	]
+	],
+	callbacks: {
+		authorized({ auth, request }) {
+			return !!auth?.user
+		}
+	}
 }
 
 export const {
