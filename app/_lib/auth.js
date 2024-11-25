e53d00f0845cdcd4d5ecd094d7a6ccb9736e526c
Thiago Morais <thiagofernandosouza20@gmail.com>
2024-11-24 22:05:11 -0300

    update: custom sign in page

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -12,10 +12,15 @@ const authConfig = {
 		authorized({ auth, request }) {
 			return !!auth?.user
 		}
+	},
+	pages: {
+		signIn: "/login"
 	}
 }
 
 export const {
 	auth,
+	signIn,
+	signOut,
 	handlers: { GET, POST }
 } = NextAuth(authConfig)
